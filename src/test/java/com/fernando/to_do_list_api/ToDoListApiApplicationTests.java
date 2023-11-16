5046fbeb955a6a6df7b3dc93978bf625c84f4181
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2023-11-16 15:00:01 -0300

    Initialize spring boot project

---
+++ src/test/java/com/fernando/to_do_list_api/ToDoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.to_do_list_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
